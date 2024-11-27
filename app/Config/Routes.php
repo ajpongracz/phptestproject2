89c44169bf1890780bdec830a8e21d7a010b07c0
AJ Pongracz <ajpongress@gmail.com>
2024-11-27 14:41:41 -0800

    Fixed bad merge

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,15 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\OtpController;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('otp', [OtpController::class, 'index']);
+$routes->get('otp/storenewpassword', [OtpController::class, 'new']);
+$routes->post('otp', [OtpController::class, 'storePw']);
+
+?>
